cb7a7f8811713ba1e95af281a18f535c654367b6
saltypanda <saltypandaforthewin@gmail.com>
2017-11-09 10:19:16 +0100

    Table hotles+typehotel with all the dependency

---
+++ src/adminBundle/Entity/HotelTypes.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace adminBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * HotelTypes
+ *
+ * @ORM\Table(name="hotel_types")
+ * @ORM\Entity(repositoryClass="adminBundle\Repository\HotelTypesRepository")
+ */
+class HotelTypes
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+
+
+    /**
+     *
+     * @ORM\OneToMany(targetEntity="adminBundle\Entity\Hotels", mappedBy="hotel_type")
+     */
+    private $type_hotel;
+
+
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    private $updatedAt;
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return HotelTypes
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return HotelTypes
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return HotelTypes
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypeHotel()
+    {
+        return $this->type_hotel;
+    }
+
+    /**
+     * @param mixed $type_hotel
+     */
+    public function setTypeHotel($type_hotel)
+    {
+        $this->type_hotel = $type_hotel;
+    }
+
+
+
+}
+
